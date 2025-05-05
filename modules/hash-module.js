a9b219464b9cdc5084d4b3fecd1f81d75e45c111
AlexGorSer <taskekaminari@gmail.com>
2025-05-05 15:26:27 +0200

    feat: add hash function

--- modules/hash-module.js
+++ modules/hash-module.js
@@ -1,5 +1,24 @@
-const calcHash = async () => {
-  console.log("hash");
+import crypto from "node:crypto";
+import { createReadStream } from "node:fs";
+import path from "node:path";
+
+const calcHash = async ([pathToFile]) => {
+  const folderPath = path.resolve(pathToFile);
+  const readStream = createReadStream(folderPath);
+  const hash = crypto.createHash('sha256');
+
+  readStream.on("data", (data) => {
+    if(data) hash.update(data);
+  });
+
+  readStream.on("end", () => {
+
+    let data = hash.digest("hex");
+
+    process.stdout.write(data);
+    process.stdout.write('\n');
+  });
+
 };
 
 export { calcHash };
