f4405b9a7f5c05f5101454dbe12e56c631d7fbf8
AlexGorSer <taskekaminari@gmail.com>
2025-05-05 15:46:17 +0200

    feat: add zlib module function

--- modules/zlib-module.js
+++ modules/zlib-module.js
@@ -1,9 +1,26 @@
-const compressFile = async () => {
-  console.log("compress");
+import { createWriteStream, createReadStream } from "node:fs";
+import { createBrotliCompress, createBrotliDecompress } from "node:zlib";
+import path from "node:path";
+import stream from "node:stream";
+
+const compressFile = async ([pathToFile, pathToCompress]) => {
+  const pathFile = path.resolve(pathToFile);
+  const pathCompress = path.resolve(pathToCompress);
+  const brotliZip = createBrotliCompress();
+  const readStream = createReadStream(pathFile);
+  const writeStream = createWriteStream(pathCompress);
+
+  await stream.promises.pipeline(readStream, brotliZip, writeStream);
 };
 
-const decompressFile = async () => {
-  console.log("decompress");
+const decompressFile = async ([pathToFile, pathToCompress]) => {
+  const pathFile = path.resolve(pathToFile);
+  const pathCompress = path.resolve(pathToCompress);
+  const brotliZip = createBrotliDecompress();
+  const readStream = createReadStream(pathFile);
+  const writeStream = createWriteStream(pathCompress);
+
+  await stream.promises.pipeline(readStream, brotliZip, writeStream);
 };
 
 export { compressFile, decompressFile };
